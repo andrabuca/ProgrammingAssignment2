c4377518d79c3551f77ceca6cb029111cfcf118d
andrabuca <andra.buca@gmail.com>
2014-05-25 00:16:23 +0200

    Assignment 2

---
+++ cachematrix.R
@@ -0,0 +1,35 @@
+## This implementation of a matrix caches the inverse of the matrix at first
+
+
+## assumes x is an invertible matrix
+## returns an interface for simple setter and getter functions
+
+makeCacheMatrix <- function(A = matrix()) {
+  inverse = NULL
+  set = function(B) {
+    A <<- B
+    inverse = NULL
+  }
+  get = function() A
+  setinverse = function(inv) inverse <<- inv
+  getinverse = function() inverse
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+}
+
+
+## returns the inverse of the CacheMatrix as created in makeCacheMatrix
+## tries to use cached inverse of the matrix if available
+
+cacheSolve <- function(A, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inv = A$getinverse()
+  if (!is.null(inv)) {
+    message('getting cached data')
+    return(A)
+  }
+  data = A$get()
+  inv = solve(data,...)
+  A$setinverse(inv)
+  inv
+  
+}
